26b44fd0170c65dc4a6b603dff0569aeba961beb
sominJoo <js06m13@gmail.com>
2024-06-12 16:54:18 +0900

    임시 커밋

--- api-gateway/src/main/java/com/mobigen/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/mobigen/apigateway/ApiGatewayApplication.java
@@ -1,14 +1,21 @@
 package com.mobigen.apigateway;
 
+import io.opentelemetry.exporter.otlp.trace.OtlpGrpcSpanExporter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.r2dbc.R2dbcAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication(exclude = {R2dbcAutoConfiguration.class})
 public class ApiGatewayApplication {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+	@Bean
+	public OtlpGrpcSpanExporter otlpHttpSpanExporter(@Value("${tracing.url}") String url) {
+		return OtlpGrpcSpanExporter.builder().setEndpoint(url).build();
 	}
 	
 }
