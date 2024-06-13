ae6531c6138260a2f6dd021aa36241a7be3b77d3
sominJoo <js06m13@gmail.com>
2024-06-13 17:14:24 +0900

    임시 커밋

--- a-service/src/main/java/com/mobigen/aservice/service/AService.java
+++ a-service/src/main/java/com/mobigen/aservice/service/AService.java
@@ -1,13 +1,29 @@
 package com.mobigen.aservice.service;
 
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.trace.Tracer;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
 
 @Slf4j
 @Service
 public class AService {
+	private static final Logger logger = LoggerFactory.getLogger(AService.class);
+	
+	@Value("${spring.application.name}")
+	private String applicationName;
+	
+	private Tracer tracer;
 	public Object getTest() {
-		log.info("A service getTest 동작");
+		logger.info("Incoming request at {} for request /path1 ", applicationName);
+		
+		
+		logger.info("A service getTest 동작");
+		
 		return "A Service 안녕";
 	}
 }
--- api-gateway/src/main/java/com/mobigen/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/mobigen/apigateway/ApiGatewayApplication.java
@@ -1,6 +1,5 @@
 package com.mobigen.apigateway;
 
-import io.opentelemetry.exporter.otlp.trace.OtlpGrpcSpanExporter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,9 +12,4 @@ public class ApiGatewayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
-	@Bean
-	public OtlpGrpcSpanExporter otlpHttpSpanExporter(@Value("${tracing.url}") String url) {
-		return OtlpGrpcSpanExporter.builder().setEndpoint(url).build();
-	}
-	
 }
